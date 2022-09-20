1c0447ec8dea0ebcb9f55b0e30fe301dfc08e800
den-nis <50838791+den-nis@users.noreply.github.com>
2022-09-20 22:36:42 +0200

    Shapes refactor

--- OpenCGL/Drawing/CanvasShapes.cs
+++ OpenCGL/Drawing/CanvasShapes.cs
@@ -4,61 +4,18 @@ namespace OpenCGL.Drawing
 {
 	public partial class Canvas
     {
-        private void DrawSquare(int x, int y, int size, Color fill)
+        public void DrawRectanlge(Vec2i point1, Vec2i point2, Color color) => DrawRectanlge(point1.X, point1.Y, point2.X, point2.Y, color);
+        public void DrawRectanlge(int x1, int y1, int x2, int y2, Color color)
         {
-            if (size == 1)
-            {
-                DrawCharacter(x, y, fill);
-                return;
-            }
-
-            int halfSize = size / 2;
-            for (int iy = -halfSize; iy <= halfSize; iy++)
-            {
-                for (int ix = -halfSize; ix <= halfSize; ix++)
-                {
-                    DrawCharacter(x + ix, y + iy, fill);
-                }
-            }
+            DrawTriangleAdvanced(x1, y1, x1, y2, x2, y2, color, false);
+            DrawTriangleAdvanced(x1, y1, x2, y1, x2, y2, color, false);    
         }
 
-        public void FillRectanlge(Vec2i point1, Vec2i point2, Color fill) => FillRectanlge(point1.X, point1.Y, point2.X, point2.Y, fill);
-        public void FillRectanlge(int x1, int y1, int x2, int y2, Color fill)
+        public void DrawLine(float x1, float y1, float x2, float y2, float lineWidth, Color color) => DrawLine(new Vec2f(x1,y1), new Vec2f(x2, y2), lineWidth, color);
+        public void DrawLine(Vec2f p1, Vec2f p2, float strokeWidth, Color color)
         {
-            var xe = Math.Max(x1, x2);
-            var ye = Math.Max(y1, y2);
-
-            for (int y = Math.Min(y1, y2); y <= ye; ++y)
-                for (int x = Math.Min(x1, x2); x <= xe; ++x)
-                    DrawCharacter(x, y, fill);
-        }
-
-        public void DrawRectangle(Vec2i point1, Vec2i point2, int strokeWidth, Color stroke) => DrawRectangle(point1.X, point1.Y, point2.X, point2.Y, strokeWidth, stroke);
-        public void DrawRectangle(int x1, int y1, int x2, int y2, int strokeWidth, Color stroke)
-        {
-            var xs = Math.Min(x1, x2);
-            var ys = Math.Min(y1, y2);
-            var xe = Math.Max(x1, x2);
-            var ye = Math.Max(y1, y2);
-
-            for (int y = ys; y <= ye; ++y)
-            {
-                DrawSquare(xs, y, strokeWidth, stroke);
-                DrawSquare(xe, y, strokeWidth, stroke);
-            }
-
-            for (int x = xs; x <= xe; ++x)
-            {
-                DrawSquare(x, ys, strokeWidth, stroke);
-                DrawSquare(x, ye, strokeWidth, stroke);
-            }
-        }
-
-        public void DrawLine(float x1, float y1, float x2, float y2, float strokeWidth, Color fill) => DrawLine(new Vec2f(x1,y1), new Vec2f(x2, y2), strokeWidth, fill);
-        public void DrawLine(Vec2f p1, Vec2f p2, float strokeWidth, Color fill)
-        {
-            float dx = (p2.X - p1.X); //Delta x
-            float dy = (p2.Y - p1.Y); //Delta y
+            float dx = (p2.X - p1.X);
+            float dy = (p2.Y - p1.Y);
 
             float angle = (float)Math.Atan2(dx, dy);
             float d90 = (float)Math.PI / 2;
@@ -66,20 +23,18 @@ namespace OpenCGL.Drawing
             Vec2f r = new Vec2f((float)Math.Sin(angle + d90) * strokeWidth, (float)Math.Cos(angle + d90) * strokeWidth); 
             Vec2f l = new Vec2f((float)Math.Sin(angle - d90) * strokeWidth, (float)Math.Cos(angle - d90) * strokeWidth); 
             
-            FillTriangle(p1 + r, p2 + l, p1 + l, fill);
-            FillTriangle(p2 + r, p2 + l, p1 + r, fill);
+            DrawTriangleAdvanced(p1 + r, p2 + l, p1 + l, color, false);
+            DrawTriangleAdvanced(p2 + r, p2 + l, p1 + r, color, false);
         }
 
-        public void DrawTriangle(Vec2f p1, Vec2f p2, Vec2f p3, int strokeWidth, Color stroke) => DrawTriangle(p1.X, p1.Y, p2.X, p2.Y, p3.X, p3.Y, strokeWidth, stroke);
-        public void DrawTriangle(float x1, float y1, float x2, float y2, float x3, float y3, float strokeWidth, Color stroke)
+        public void DrawTriangle(float x1, float y1, float x2, float y2, float x3, float y3, Color color) => DrawTriangle(new Vec2f(x1,y1), new Vec2f(x2,y2), new Vec2f(x3,y3), color);
+        public void DrawTriangle(Vec2f p1, Vec2f p2, Vec2f p3, Color color)
         {
-            DrawLine(x1, y1, x2, y2, strokeWidth, stroke);
-            DrawLine(x2, y2, x3, y3, strokeWidth, stroke);
-            DrawLine(x3, y3, x1, y1, strokeWidth, stroke);
+            DrawTriangleAdvanced(p1, p2, p3, color, true);
         }
 
-        public void FillTriangle(float x1, float y1, float x2, float y2, float x3, float y3, Color fill) => FillTriangle(new Vec2f(x1,y1), new Vec2f(x2,y2), new Vec2f(x3,y3), fill);
-        public void FillTriangle(Vec2f p1, Vec2f p2, Vec2f p3, Color fill)
+        private void DrawTriangleAdvanced(float x1, float y1, float x2, float y2, float x3, float y3, Color color, bool preventOverlap) => DrawTriangleAdvanced(new Vec2f(x1,y1), new Vec2f(x2,y2), new Vec2f(x3,y3), color, preventOverlap);
+        private void DrawTriangleAdvanced(Vec2f p1, Vec2f p2, Vec2f p3, Color color, bool preventOverlap)
         {
             p1 = Context.Apply(p1);
             p2 = Context.Apply(p2);
@@ -108,57 +63,36 @@ namespace OpenCGL.Drawing
 
                     if (w0 >= 0 && w1 >= 0 && w2 >= 0) 
                     {
-                        //Make sure edges don't overlap
                         if (
-                         (w0 != 0 || (p3.Y < p2.Y || (p3.Y == p2.Y && p3.X < p2.X))) &&
-                         (w1 != 0 || (p1.Y < p3.Y || (p1.Y == p3.Y && p1.X < p3.X))) &&
-                         (w2 != 0 || (p2.Y < p1.Y || (p2.Y == p1.Y && p2.X < p1.X)))
+                            !preventOverlap 
+                            || 
+                            (
+                                (w0 != 0 || (p3.Y < p2.Y || (p3.Y == p2.Y && p3.X < p2.X))) &&
+                                (w1 != 0 || (p1.Y < p3.Y || (p1.Y == p3.Y && p1.X < p3.X))) &&
+                                (w2 != 0 || (p2.Y < p1.Y || (p2.Y == p1.Y && p2.X < p1.X)))
+                            )
                         )
                         {
-                            DrawCharacter(x, y, fill);
+                            DrawCharacter(x, y, color);
                         }
                     }
                 }
             }
         }
 
-        public void FillCircle(Vec2i point, float radius, Color fill) => FillCircle(point.X, point.Y, radius, fill);
-        public void FillCircle(int x, int y, float radius, Color fill)
+        public void DrawCircle(int x, int y, float radius, Color color, int sections = 32) => DrawCircle(new Vec2f(x,y), radius, color, sections);
+        public void DrawCircle(Vec2f point, float radius, Color color, int sections = 32)
         {
-            for (int iy = 0; iy < (int)radius + 1; iy++)
+            for (int i = 0; i < sections; i++)
             {
-                for (int ix = 0; ix < (int)radius + 1; ix++)
-                {
-                    var distance = Math.Sqrt(ix * ix + iy * iy);
+                float twoPi = (float)Math.PI * 2f;
+                float firstAngle = twoPi / sections * i; 
+                float secondAngle = twoPi / sections * (i + 1);
 
-                    if (distance < radius + .5f)
-                    {
-                        DrawCharacter(x + ix, y + iy, fill);
-                        DrawCharacter(x - ix, y + iy, fill);
-                        DrawCharacter(x + ix, y - iy, fill);
-                        DrawCharacter(x - ix, y - iy, fill);
-                    }
-                }
-            }
-        }
-
-        public void DrawCircle(Vec2i point, float radius, int strokeWidth, Color stroke) => DrawCircle(point.X, point.Y, radius, strokeWidth, stroke);
-        public void DrawCircle(int x, int y, float radius, int strokeWidth, Color stroke)
-        {
-            for (int iy = 0; iy < (int)radius + 1; iy++)
-            {
-                for (int ix = 0; ix < (int)radius + 1; ix++)
-                {
-                    var distance = Math.Sqrt(ix * ix + iy * iy);
-
-                    if (distance < radius + .5f && distance >= radius - 1)
-                    {
-                        DrawSquare(x + ix, y + iy, strokeWidth, stroke);
-                        DrawSquare(x - ix, y + iy, strokeWidth, stroke);
-                        DrawSquare(x + ix, y - iy, strokeWidth, stroke);
-                        DrawSquare(x - ix, y - iy, strokeWidth, stroke);
-                    }
-                }
+                Vec2f p1 = new(point.X + (float)Math.Cos(firstAngle) * radius, point.Y + (float)Math.Sin(firstAngle) * radius);
+                Vec2f p2 = new(point.X + (float)Math.Cos(secondAngle) * radius, point.Y + (float)Math.Sin(secondAngle) * radius);
+                
+                DrawTriangleAdvanced(point, p1, p2, color, false);
             }
         }
 
